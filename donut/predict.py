d65178a694a9be03c21896caa83e84d5cade690b
Jakob <jakob.petek@hotmail.com>
2025-12-27 13:27:28 +0100

    Update predict.py

--- donut/predict.py
+++ donut/predict.py
@@ -1,7 +1,53 @@
 from transformers import DonutProcessor, VisionEncoderDecoderModel
+from datasets import load_from_disk
 import torch
 from PIL import Image
-import re
+
+
+def get_processed_dataset(dataset_type, test_size=0.1, validation_size=0.1, debug=False, seed=42):
+    processed_dataset = load_from_disk(dataset_type + "-dataset")
+
+    if debug:
+        sample = processed_dataset[0]
+        img_list = sample["pixel_values"]
+        img_tensor = torch.tensor(img_list, dtype=torch.float32)
+        print(f"Image tensor shape: {img_tensor.shape}")
+
+    train_test_split = processed_dataset.train_test_split(
+        test_size=test_size, seed=seed
+    )
+
+    train_val_split = train_test_split["train"].train_test_split(
+        test_size=validation_size, seed=seed
+    )
+
+    # Step 3: Combine all splits into one dict
+    processed_dataset = {
+        "train": train_val_split["train"],
+        "validation": train_val_split["test"],
+        "test": train_test_split["test"]
+    }
+
+    return processed_dataset
+
+
+def get_processor(dataset_type):
+    proc = DonutProcessor.from_pretrained(dataset_type + "-processor")
+    return proc
+
+
+def get_model(dataset_type, checkpoint_path=""):
+    model = VisionEncoderDecoderModel.from_pretrained(dataset_type + "-model" + checkpoint_path)
+
+    return model
+
+
+def get_device():
+    return "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def check_model_processor_compatibility(model, processor):
+    assert len(processor.tokenizer) == model.config.decoder.vocab_size
 
 
 def run_prediction(sample, model, processor, device, has_target=True):
@@ -32,20 +78,8 @@ def run_prediction(sample, model, processor, device, has_target=True):
         return prediction, None
 
     # load reference target
-    target = processor.token2json(sample["target_sequence"])
+    target = processor.token2json(sample["target_sequence"])  # What is here target_sequence?
     return prediction, target
-
-
-def load_model_and_processor():
-    processor = DonutProcessor.from_pretrained("donut_ocr/processor_ocr_ingredients")
-    model = VisionEncoderDecoderModel.from_pretrained("donut_ocr/outputs/donut-ocr-ingredients" + "/checkpoint-19000")
-
-    assert len(processor.tokenizer) == model.config.decoder.vocab_size
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model.to(device)
-
-    return model, processor, device
 
 
 def run_prediction_from_image(
@@ -96,19 +130,22 @@ def run_prediction_from_image(
     return prediction
 
 
-def clean_donut_text(prediction_json):
-    """
-    Extract text_sequence and remove <s> </s> tokens.
-    """
-    if not isinstance(prediction_json, dict):
-        return ""
+if __name__ == "__main__":
+    # Runs prediction on test set from his dataset not arbitrary image
+    data_type = "sroie"
 
-    text = prediction_json.get("text_sequence", "")
+    # Load processor and model, move model to device
+    processor = get_processor(data_type)
+    model = get_model(data_type)
+    device = get_device()
 
-    # Remove <s> and </s> tokens
-    text = re.sub(r"</?s>", "", text)
+    check_model_processor_compatibility(model, processor)
+    model.to(device)
 
-    # Normalize whitespace
-    text = re.sub(r"\s+", " ", text).strip()
+    # Grab the first sample from the processed test section of dataset
+    test_sample = get_processed_dataset(data_type)["test"][0]
 
-    return text
+    # Run prediction
+    prediction, target = run_prediction(test_sample, model, processor, device)
+    print("Prediction:", prediction)
+    print("Target:", target)
