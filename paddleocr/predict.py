470f67358963a5e1966c517a7f6dbfa2ab4e9664
Jakob <jakob.petek@hotmail.com>
2026-01-11 12:29:29 +0100

    Added simple paddle ocr example

--- paddleocr/predict.py
+++ paddleocr/predict.py
@@ -1,4 +1,34 @@
-# TODO: add PaddleOCR prediction code here
+from paddleocr import PaddleOCR
+import os
 
-def run_prediction(image, debug=False, config=None):
-    raise NotImplementedError("PaddleOCR prediction code is not yet implemented.")
+from common.helpers import create_folder, get_data
+
+
+def run_prediction(image_path, debug=False):
+    ocr = PaddleOCR(
+        use_doc_orientation_classify=False,
+        use_doc_unwarping=False,
+        use_textline_orientation=False)
+
+    # Run OCR inference on a sample image
+    result = ocr.predict(input=image_path)
+
+    # Visualize the results and save the JSON results
+    for res in result:
+        if debug:
+            res.save_to_img("output")
+            # res.save_to_json("output")
+    return result
+
+
+if __name__ == "__main__":
+    img = "003.jpg"
+
+    for image_path, ground_truth in get_data("demo"):
+        if os.path.basename(image_path) != img:
+            continue
+
+        text = run_prediction(image_path, debug=True)
+        print("Extracted Text:", text)
+        print("Ground Truth:", ground_truth)
+
